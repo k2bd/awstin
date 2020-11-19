f831761ef02995cf2521552f901b644f682a7618
Kevin Duff <kevinkelduff@gmail.com>
2020-11-19 00:10:05 +0000

    Bump to 0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ description = "Utilities for building and testing AWS applications in Python"
 
 setuptools.setup(
     name="awstin",
-    version="0.0.3",
+    version="0.0.4",
     author="Kevin Duff",
     description=description,
     long_description=long_description,
